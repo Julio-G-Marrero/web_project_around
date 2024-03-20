d0334f013a6e7d59d354a18edb8cde55d022b797
JulioGMarrero <julio.garcia135@hotmail.com>
2024-03-19 20:57:46 -0600

    correcion clases

--- js/index.js
+++ js/index.js
@@ -2,8 +2,8 @@ let profileAddButton = document.querySelector(".profile__add-place");
 let profileEdit = document.querySelector(".profile__button");
 let modalProfile = document.querySelector(".modal-perfil");
 let modalPlace = document.querySelector(".modal-place");
-let modalCloseProfile = document.querySelector(".modal__close--perfil");
-let modalClosePlace = document.querySelector(".modal__close--place");
+let modalCloseProfile = document.querySelector(".modal__close-perfil");
+let modalClosePlace = document.querySelector(".modal__close-place");
 function mostrarModal(modal) {
   modal.classList.toggle("disabled");
 }
